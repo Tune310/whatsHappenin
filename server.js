aa742f2cbd4e3ca606826520b12cd78603f11ea1
Tune <Reidcrawford55@gmail.com>
2016-04-03 16:27:38 -0700

    got express body-parser andpath droppin it like it hotgit add -A

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 // Starting Project
+var
+  express = require('express'),
+  app = express(),
+  logger = require('morgan'),
+  mongoose = require('mongoose'),
+  path = require('path'),
+  bodyParser = require('body-parser')
+
+  //connect mongoose here
+
+  app.use(bodyParser.urlencoded({extended:false}))
+  app.use(bodyParser.json())
+  app.use(express.static(path.join(__dirname, 'public')))
+
+  app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+  })
+
+  //use apiRoutes here
+
+  app.listen('3000', function(){
+    console.log('Server is firing on port 3000')
+  })
