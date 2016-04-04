44f3f1257c01db0eda147bd133980c56828818a9
Tune <Reidcrawford55@gmail.com>
2016-04-03 17:50:27 -0700

    created request and Create in my node controller for users

--- controllers/api.js
+++ controllers/api.js
@@ -3,5 +3,38 @@
 var User = require('../models/User.js')
 
 module.exports = {
-  
+
+  //Show All Users
+  index: function(req, res){
+    User.find({}, function(error, users){
+      if(err) return console.log(err)
+      res.json(users)
+    })
+  },
+
+  //Create User
+  create: function(req, res){
+    User.create(req.body, function(err, user){
+      if(err) return console.log(err)
+      res.json({success: true, message: "A new user has been created and added to database", user: user})
+    })
+  },
+
+  //Show user
+  show: function(req, res){
+    User.findOne({_id: req.params.id}, 'email name', function(err, user){
+      if(err) return console.log(err)
+      res.json(user)
+    })
+  }
+
+  // update user
+
+
+
+  //delete user
+
+
 }
+
+// create a user when finished
