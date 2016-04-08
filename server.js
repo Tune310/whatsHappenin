a1c3dbf596311230571633f3156896cecba65fd0
Tune <Reidcrawford55@gmail.com>
2016-04-08 01:16:50 -0700

    HAhaha Phillie this one for you

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var
   mongoose = require('mongoose'),
   path = require('path'),
   bodyParser = require('body-parser'),
+  SC= require('node-soundcloud'),
   apiRoutes = require('./routes/api.js'),
   jwt = require('jsonwebtoken'), // double check with phillipe to make sure if I need to require this in my routes file
   config = require('./config.js') // config file for jwt
@@ -17,14 +18,32 @@ var
     console.log('Mongodb is now connected and firing')
   })
 
+
   app.set('superSecret', config.secret)
 
   app.use(bodyParser.urlencoded({extended:false}))
   app.use(bodyParser.json())
   app.use(express.static(path.join(__dirname, 'public')))
 
+  SC.init({
+    id: '030341538cff3ba796885fa35911cb51',
+    secret: '030341538cff3ba796885fa35911cb51',
+   })
+
   app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, 'public/index.html'))
+  })
+
+  app.get('/test/:artist', function(req, res){
+    //Soundcloud requests tracks under this artist
+    SC.get('/tracks', {q: req.params.artist}, function(err, track) {
+  if ( err ) {
+    throw err;
+  } else {
+    // if the input is true json will set us data under the input
+    res.json('track retrieved:', track);
+    }
+  });
   })
 
  // using this for test at the moment
